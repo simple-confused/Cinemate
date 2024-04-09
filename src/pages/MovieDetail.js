8bdee3059ffee8834e28efc69d60c7d04ff58161
Aryan Chaturvedi <mandaknidevi04@gmail.com>
2024-04-09 20:25:53 +0530

    modified

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -5,6 +5,7 @@ import Backup from "../assets/images/backup.png";
 export const MovieDetail = () => {
   const params = useParams();
   const [movie, setMovie] = useState({});
+  //eslint-disable-next-line
   const image = movie.poster_path
     ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
     : Backup;
